6bc86a3e8c619c119837ff476f81b747ea14fc8d
Lukas Bystricky <lb13f@my.fsu.edu>
2016-05-30 16:20:00 -0400

    added Fortran test code to test OpenMP

---
+++ stokes_fmm/FMM_TEST.f90
@@ -0,0 +1,28 @@
+PROGRAM FMM_TEST
+
+use omp_lib
+
+IMPLICIT NONE
+
+INTEGER :: nsource, i
+REAL *8, DIMENSION(100000) :: xs
+REAL *8, DIMENSION(100000) :: ys
+Real :: ostart, oend
+COMPLEX *16, DIMENSION(100000) :: dip1
+COMPLEX *16, DIMENSION(100000) :: dip2
+COMPLEX *16, DIMENSION(100000) :: vel
+
+nsource = 100000
+xs = (/(i*0.1, i = 1,nsource)/)
+ys = (/(i*0.1, i = 1,nsource)/)
+dip1 = (/((1,1), i = 1,nsource)/)
+dip2 = (/((1,1), i = 1,nsource)/)
+
+ostart = omp_get_wtime()
+
+call stokesDLPnew(nsource,xs,ys,dip1,dip2,vel)
+
+oend = omp_get_wtime()
+
+write(*,*) 'Walltime ellapsed', oend-ostart
+END PROGRAM
