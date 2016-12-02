d822af4c57752d2a6419178e934e9ed522d61b2a
Lukas Bystricky <lb13f@my.fsu.edu>
2016-12-02 14:58:33 -0500

    shear flow example

--- examples/shear.m
+++ examples/shear.m
@@ -1,8 +1,6 @@
 close all
 
-%% create animated gif and plots
-
-prams.N = 128; % points per body
+prams.N = 32; % points per body
 prams.Nbd = 0; %points on solid wall
 
 g = -5;%shear rate
@@ -12,8 +10,8 @@ prams.nbd = 0; %number of walls
 prams.lengths = ones(1,prams.nv);
 prams.widths = 0.25*ones(1,prams.nv);
 prams.T = 10*pi*(prams.lengths/prams.widths + prams.widths/prams.lengths)/abs(g);
-prams.m = 800; % number of time steps
-prams.order = 6;
+prams.m = 200; % number of time steps
+prams.order = 4;
 prams.tracker_fnc = @(t) [20*cos(t),20*sin(t);5*cos(t),5*sin(t)];
 
 options.farField = 'shear';
@@ -38,10 +36,16 @@ Xfinal = rigid2D(options, prams, xc, tau, xWalls);
 
 pp = post(['../output/data/',options.fileBase, '.mat']);
 
-a = prams.widths/2;
-b = prams.lengths/2;
-tau_exact = @(t) atan((b/a)*tan(a*b*g*t/(a^2+b^2)));
-omega_exact = @(t) g/(a^2+b^2)*(b^2*cos(tau_exact(t)).^2 + a^2*sin(tau_exact(t)).^2);
-
-pp.animated_gif('shear_8_fibers', 'tikz', 4, [], 'fibres')
+gif_options.file_name = 'shear';
+gif_options.file_type = 'gif';
+gif_options.plot_fluid = false;
+gif_options.xmin = 'auto:frame';
+gif_options.xmax = 'auto:frame';
+gif_options.ymin = 'auto:all';
+gif_options.ymax = 'auto:all';
+gif_options.axis = true;
+gif_options.itmax = prams.m;
+gif_options.stride = 1;
+    
+pp.animated_gif(gif_options)
 
