be298e39ab41e8dee06a5e73cd3b0ad493fe7500
Lukas Bystricky <lb13f@my.fsu.edu>
2016-12-02 13:17:57 -0500

    example for single particle Jeffery orbit

--- examples/jeffery.m
+++ examples/jeffery.m
@@ -1,8 +1,7 @@
 close all
 
 %% create animated gif and plots
-
-if 0
+if 1
     prams.N = 64; % points per body
     prams.Nbd = 0; %points on solid wall
 
@@ -13,7 +12,7 @@ if 0
     prams.lengths = 1;
     prams.widths = 0.25;
     prams.T = pi*(prams.lengths/prams.widths + prams.widths/prams.lengths)/abs(g);
-    prams.m = 400; % number of time steps
+    prams.m = 100; % number of time steps
     prams.order = 2;
     prams.tracker_fnc = @(t) [20*cos(t),20*sin(t);5*cos(t),5*sin(t)];
 
@@ -22,7 +21,7 @@ if 0
     options.fileBase = 'jeffery';
     options.append = false;
     options.inear = true;
-    options.usePreco = true;
+    options.usePreco = false;
     options.ifmm = true;
     options.verbose = true;
     options.profile = false;
@@ -35,37 +34,44 @@ if 0
     xc = [0;0];
     tau  = pi/2;
 
-    %Xfinal = rigid2D(options, prams, xc, tau, xWalls);
+    Xfinal = rigid2D(options, prams, xc, tau, xWalls);
 
     pp = post(['../output/data/',options.fileBase, '.mat']);
 
     a = prams.widths/2;
     b = prams.lengths/2;
-    theta_exact = @(t) atan((b/a)*tan(a*b*g*t/(a^2+b^2)));
-    omega_exact = @(t) g/(a^2+b^2)*(b^2*cos(theta_exact(t)).^2 + a^2*sin(theta_exact(t)).^2);
+    tau_exact = @(t) atan((b/a)*tan(a*b*g*t/(a^2+b^2)));
+    omega_exact = @(t) g/(a^2+b^2)*(b^2*cos(tau_exact(t)).^2 + a^2*sin(tau_exact(t)).^2);
 
-    % 
-    % plot(pp.times, wrapTo2Pi(pp.tau));
-    % hold on
-    % plot(pp.times, pp.omega);
-    % fplot(@(t) theta_exact(t) + pi/2, [0, prams.T]);
-    % fplot(@(t) omega_exact(t), [0,prams.T]);
-    % 
-    % legend('angle', 'angular velocity', 'Jeffery angle', 'Jeffery velocity');
+    
+    fplot(@(t) tau_exact(t) + pi/2, [0, prams.T]);
+    hold on
+    fplot(@(t) omega_exact(t), [0,prams.T]);
+    plot(pp.times, wrapTo2Pi(pp.tau), 'bo');
+    plot(pp.times(1:end-1), pp.omega, 'ro');
+    
+    legend({'$\theta$ (Jeffery)', '$\omega$ (Jeffery)', '$\theta$ (BIE)', '$\omega$ (BIE)'}, 'interpreter', 'latex');
+    xlabel('$t$', 'interpreter', 'latex');
 
-
-    %pp.animated_gif('jefery_orbit.gif', 3, [], 'fluid')
+    addpath('../tests/matlab2tikz/src');
+    matlab2tikz('jeffery.tex', 'height', '10cm', 'width', '12cm');
+    
+    pp.animated_gif('jeffery_orbit', 'tikz', 1, [], 'fluid')
 end
 
 %% convergence study
-if 1
+if 0
     
-    M = 5;
-    errors = zeros(M,1);
+    M = 4;
+    errors_fe = zeros(M,1);
+    errors_ab = zeros(M,1);
+    dt = zeros(M,1);
+    
     for i = 1:M
         
-        disp(['dT = ', num2str(1/(2^(i+1)))]);
-        prams.N = 256; % points per body
+        dt(i) = 1/(2^(i+3));
+        disp(['dT = ', num2str(1/(2^(i+2)))]);
+        prams.N = 128; % points per body
         prams.Nbd = 0; %points on solid wall
 
         g = -5;%shear rate
@@ -74,8 +80,8 @@ if 1
         prams.nbd = 0; %number of walls
         prams.lengths = 1;
         prams.widths = 0.25;
-        prams.T = 1;
-        prams.m = 2^(i + 1); % number of time steps
+        prams.T = 0.125;
+        prams.m = prams.T*(2^(i + 3)); % number of time steps
         prams.order = 2;
         prams.tracker_fnc = @(t) [20*cos(t),20*sin(t);5*cos(t),5*sin(t)];
 
@@ -88,7 +94,7 @@ if 1
         options.ifmm = true;
         options.verbose = true;
         options.profile = false;
-        options.tstep_order = 2;
+        options.tstep_order = 1;
         options.confined = false;
 
         [options,prams] = initRigid2D(options,prams);
@@ -103,14 +109,27 @@ if 1
 
         a = prams.widths/2;
         b = prams.lengths/2;
-        theta_exact = @(t) atan((b/a)*tan(a*b*g*t/(a^2+b^2)));
-        omega_exact = @(t) g/(a^2+b^2)*(b^2*cos(theta_exact(t)).^2 + a^2*sin(theta_exact(t)).^2);
+        tau_exact = @(t) atan((b/a)*tan(a*b*g*t/(a^2+b^2)));
+        omega_exact = @(t) g/(a^2+b^2)*(b^2*cos(tau_exact(t)).^2 + a^2*sin(tau_exact(t)).^2);
         
-        errors(i) = abs(omega_exact(prams.T) - pp.omega(end));
+        errors_fe(i) = abs(tau_exact(pp.times(end)) - pp.tau(end) + pi/2);
+
+        options.tstep_order = 2;
+        Xfinal = rigid2D(options, prams, xc, tau, xWalls);
+
+        pp = post(['../output/data/',options.fileBase, '.mat']);
+        errors_ab(i) = abs(tau_exact(pp.times(end)) - pp.tau(end) + pi/2);
     end
     
-    rates = zeros(M-1,1);
-    for i = 1:M-1
-        rates(i) = log(errors(i)/errors(i+1))/log(2);
-    end
+    figure();
+    loglog(dt, errors_fe, 'bo', 'linewidth', 2);
+    hold on
+    loglog(dt, errors_ab, 'ro', 'linewidth', 2);
+
+    loglog([dt(1), dt(end)], [errors_fe(1), errors_fe(1)/(2^(length(dt)-1))], 'b');
+    loglog([dt(1), dt(end)], [errors_ab(1), errors_ab(1)/(4^(length(dt)-1))], 'r');
+    
+    legend({'forward Euler', 'Adams Bashforth', '$O(\Delta t)$', '$O(\Delta t^2)$'}, 'interpreter', 'latex');
+    xlabel('$\Delta t$', 'interpreter', 'latex');
+    ylabel('error in $\theta$', 'interpreter', 'latex');
 end
