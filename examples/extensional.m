da652db3bfe81863d109d454cfa92320d012d21b
Lukas Bystricky <lb13f@my.fsu.edu>
2016-12-06 20:36:49 -0500

    extensional flow example

--- examples/extensional.m
+++ examples/extensional.m
@@ -2,8 +2,8 @@ close all
 
 prams.N = 64; % points per body
 prams.nv = 2; % number of bodies
-prams.T = 5; % time horizon
-prams.m = 100; % number of time steps
+prams.T = 10; % time horizon
+prams.m = 200; % number of time steps
 prams.lengths = 1*ones(1, prams.nv);
 prams.widths = 1*ones(1,prams.nv);
 prams.order = 2;
@@ -22,7 +22,7 @@ options.confined = false;
 
 [options,prams] = initRigid2D(options,prams);
 
-xc = [0, 0.05; 0.6, -0.6];
+xc = [0, 0.001; 0.6, -0.6];
 tau = [pi/2,pi/2];
 
 Xfinal = rigid2D(options, prams, xc, tau);
