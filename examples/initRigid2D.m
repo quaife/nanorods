8f0871a008b65e2b88331eb9a10e4ff2cfd8106c
Lukas Bystricky <lb13f@my.fsu.edu>
2017-03-08 11:39:58 -0500

    cleaned up code

--- examples/initRigid2D.m
+++ examples/initRigid2D.m
@@ -8,13 +8,15 @@ if isempty(strfind(P, subPath))
   addpath(subPath)
 end
 
-PramList = {'N','nv','order', 'T', 'm','gmresTol'};
-defaultPram.N = 64;
-defaultPram.nv = 1;
+PramList = {'Np','np', 'Nw', 'nw', 'lengths', 'widths', 'rounding_order', ...
+                        'T', 'number_steps'};
+defaultPram.Np = 64;
+defaultPram.np = 1;
+defaultPram.Nw = 0;
+defaultPram.nw = 0;
 defaultPram.T = 1;
-defaultPram.m = 10;
-defaultPram.gmresTol = 1e-6;
-defaultPram.order = 4;
+defaultPram.number_steps = 10;
+defaultPram.rounding_order = 2;
 for k = 1:length(PramList)
   if ~isfield(prams,PramList{k})
     eval(['prams.' PramList{k} '=defaultPram.' PramList{k} ';'])
@@ -23,16 +25,26 @@ for k = 1:length(PramList)
 end
 
 
-OptionList = {'tstep_order','inear','farField','verbose','usePreco','ifmm', ...
-        'append', 'profile'};
-defaultOption.tstep_order = 1;
-defaultOption.inear = true;
-defaultOption.farField = 'shear';
+OptionList = {'tstep_order','near_singular','far_field','verbose','use_precond','fmm', ...
+        'append', 'gmres_tol', 'profile', 'save_data', 'confined', 'rk_tol', 'rk_min_dt', ...
+        'rk_max_up', 'rk_max_down', 'rk_safety'};
+    
+defaultOption.tstep_order = 2;
+defaultOption.near_singular = true;
+defaultOption.far_field = 'shear';
 defaultOption.verbose = true;
-defaultOption.usePreco = false;
+defaultOption.use_precond = false;
 defaultOption.fmm = false;
 defaultOption.append = false;
 defaultOption.profile = false;
+defaultOption.gmres_tol = 1e-6;
+defaultOption.confined = false;
+defaultOption.save_data = true;
+defaultOption.rk_tol = 1e-2;
+defaultOption.rk_min_dt = 1e-5;
+defaultOption.rk_max_up = 1.5;
+defaultOption.rk_max_down = 0.5;
+defaultOption.rk_safety = 0.9;
 
 for k = 1:length(OptionList)
   if ~isfield(options,OptionList{k})
