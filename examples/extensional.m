3f8839bedbcdfaf964f857aaf73aa384fb44c458
Lukas Bystricky <lb13f@my.fsu.edu>
2017-07-31 16:42:32 -0400

    extensional.m: addded correct options

--- examples/extensional.m
+++ examples/extensional.m
@@ -1,9 +1,9 @@
 close all
 
-prams.Np = 32; % points per body
+prams.Np = 128; % points per body
 prams.np = 2; % number of bodies
 prams.T = 5; % time horizon
-prams.number_steps = 500; % number of time steps
+prams.number_steps = 200; % number of time steps
 prams.lengths = 1;
 prams.widths = 0.125;
 prams.rounding_order = 2;
@@ -15,10 +15,10 @@ options.file_base = 'extensional_rk45';
 options.append = false;
 options.near_singular = true;
 options.use_precond = true;
-options.fmm = true;
+options.fmm = false;
 options.verbose = true;
 options.profile = false;
-options.tstep_order = 2;
+options.tstep_order = 1;
 options.confined = false;
 options.rk_tol = 1e-2;
 options.rk_dt_min = 1e-2;
@@ -28,23 +28,26 @@ options.rk_safety = 0.9;
 options.resolve_collisions = true;
 options.display_solution = true;
 options.debug = false;
+options.explicit = true;
 
 [options,prams] = initRigid2D(options,prams);
 
 % xc = [-1.95, -1.3, -0.65, 0, 0.65, 1.3, 1.95; 0.5, -0.5, 0.5, -0.5, 0.5, -0.5, 0.5];
 % tau = [pi/2,pi/2,pi/2,pi/2,pi/2,pi/2,pi/2];
-xc = [0,0 ; 0.6, 0];
-tau = [0.95*pi/2, 0];
+% xc = [0,0,0,0 ;2.5, 0.6, 0, -2.5];
+% tau = [pi/2, 0, pi/2, pi/2];
+xc = [0, 0; 0.7, 0];
+tau = [0,pi/2];
 % xc = [-0.6, -0.2, 0.2, 0.6; 0, 0, 0, 0];
 % tau = pi/4*ones(1,prams.np);
-%xc = [1;0];
-%tau = pi/2*ones(1,prams.np);
+% xc = [1;0];
+% tau = pi/2*ones(1,prams.np);
 
 Xfinal = rigid2DCollisions(options, prams, xc, tau);
 
 pp = post(['../output/data/',options.file_base, '.mat']);
 
-gif_options.file_name = 'collision_resolved_N64_ellipse1';
+gif_options.file_name = 'extensional_explicit_64';
 gif_options.file_type = 'gif';
 gif_options.plot_fluid = false;
 gif_options.plot_pressure = false;
@@ -54,7 +57,7 @@ gif_options.ymin = 'auto:all';
 gif_options.ymax = 'auto:all';
 gif_options.axis = true;
 gif_options.itmax = prams.number_steps;
-gif_options.stride = 1;
+gif_options.stride = 10;
 gif_options.contour_field = [];    
 gif_options.velocity_quiver = false;
 pp.animatedGifExtended(gif_options);
