1a55f3b6947a2d5ad6b2456510c86e2c0d2ee5e0
Lukas Bystricky <lukasby@kth.se>
2019-01-28 09:48:00 +0100

    code to add correct directories to path

---
+++ create_paths.m
@@ -0,0 +1,5 @@
+addpath('examples');
+addpath('src');
+addpath('stokes_fmm');
+addpath('laplace_fmm');
+setenv('LD_RUN_PATH', '/afs/kth.se/home/l/u/lukasby/Documents/Research/nanorods/src/iagm/iagm');
